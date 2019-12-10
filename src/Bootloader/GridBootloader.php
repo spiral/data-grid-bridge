5bb3d7f5cc0f10715f696b17e4cdebed97790719
Wolfy-J <wolfy.jd@gmail.com>
2019-12-10 16:22:03 +0300

    - bump bridge version to 1.0

--- src/Bootloader/GridBootloader.php
+++ src/Bootloader/GridBootloader.php
@@ -1,15 +1,19 @@
 <?php
+
 /**
  * Spiral Framework.
  *
  * @license   MIT
  * @author    Anton Titov (Wolfy-J)
  */
+
 declare(strict_types=1);
 
 namespace Spiral\DataGrid\Bootloader;
 
+use Psr\Container\ContainerInterface;
 use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Database\DatabaseInterface;
 use Spiral\DataGrid\Compiler;
 use Spiral\DataGrid\Grid;
 use Spiral\DataGrid\GridFactory;
@@ -30,10 +34,13 @@ final class GridBootloader extends Bootloader
     /**
      * @return Compiler
      */
-    public function compiler(): Compiler
+    public function compiler(ContainerInterface $container): Compiler
     {
         $compiler = new Compiler();
-        $compiler->addWriter(new QueryWriter());
+
+        if ($container->has(DatabaseInterface::class)) {
+            $compiler->addWriter(new QueryWriter());
+        }
 
         return $compiler;
     }
---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Spiral Framework. PHP Data Grid
+ *
+ * @license MIT
+ * @author  Anton Tsitou (Wolfy-J)
+ * @author  Valentin Vintsukevich (vvval)
+ */
+
+declare(strict_types=1);
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', '1');
+
+//Composer
+require dirname(__DIR__) . '/vendor/autoload.php';
